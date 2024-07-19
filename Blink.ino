baf14d038ffdc96b0a59d6ef9f6d0018eba9a0f0
embk <hyperwoven@gmail.com>
2024-07-19 07:28:00 +0200

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,19 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+*/
+int ledPin = 36; 
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
